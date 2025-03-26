b1581f3d3bc46398e8d31ea4f349cf9d5175eda9
ericsybesma <esybesma@gmail.com>
2025-03-26 12:59:13 -0500

    Update mongo_connect.go

--- database/mongo_connect.go
+++ database/mongo_connect.go
@@ -12,7 +12,7 @@ func MongoConnect() (*mongo.Client, error) {
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(
-		"mongodb+srv://promptDbUser:PdoNcJ3DzUWu42FP@promptdbaws.f7iaa.mongodb.net/" +
+		"mongodb+srv://xxxxx@promptdbaws.f7iaa.mongodb.net/" +
 			"?retryWrites=true&w=majority&appName=PromptDbAws").SetServerAPIOptions(serverAPI)
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
